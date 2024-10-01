4bf5e6bd65cc1d20a0d6ea3a91a79499aabb75cf
Gregory Boyd <gboyd69@yahoo.com>
2024-09-30 20:02:03 -0400

    added google font files

--- app/(auth)/sign_in.jsx
+++ app/(auth)/sign_in.jsx
@@ -190,6 +190,7 @@ const styles = StyleSheet.create({
   subHeader: {
     color: 'white',
     marginTop: 5,
+    fontFamily: 'Urbanist-Regular',
   },
   buttonContainer: {
     width: '75%',
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,7 +1,27 @@
-import { Stack } from 'expo-router';
+import { Stack, SplashScreen } from 'expo-router';
+import { useFonts } from 'expo-font';
 import GlobalProvider from '../context/GlobalProvider';
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
+  const [loaded, error] = useFonts({
+    'Urbanist-Regular': require('../assets/fonts/Urbanist-Regular.ttf'),
+    'Urbanist-Medium': require('../assets/fonts/Urbanist-Medium.ttf'),
+    'ConcertOne-Regular': require('../assets/fonts/ConcertOne-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <GlobalProvider>
       <Stack
