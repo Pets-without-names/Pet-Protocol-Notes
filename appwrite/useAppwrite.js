51c40994684c85b809008c9d9f1e5c33d8cff8c8
Gregory Boyd <gboyd69@yahoo.com>
2024-10-08 08:36:01 -0400

    removed old comments in useAppwrite

--- appwrite/useAppwrite.js
+++ appwrite/useAppwrite.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import { Alert } from 'react-native';
 
 const useAppwrite = (fn) => {
   const [data, setData] = useState([]);
@@ -18,29 +17,12 @@ const useAppwrite = (fn) => {
     }
   };
 
-  //function to fetch individual dog note detail after the
-  //user updates the note:
-  const fetchNote = async (id) => {
-    setIsLoading(true);
-    try {
-      const response = await fn();
-      setData(response.documents);
-    } catch (error) {
-      console.log('refetch note error: ' + error);
-      throw new Error(error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
   useEffect(() => {
-    fetchNote();
     fetchNotes();
   }, []);
 
-  const refetchNote = () => fetchNote();
   const refetch = () => fetchNotes();
-  return { data, isLoading, refetch, refetchNote };
+  return { data, isLoading, refetch };
 };
 
 export default useAppwrite;
